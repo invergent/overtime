4a5f5ecf0d4c7be5fefc75bef88d1553a756edae
Invergent Technologies <invergenttechnologies@gmail.com>
2019-05-31 18:22:55 +0100

    enable admin access on staff profile and image upload routes

--- src/Application/Features/ProfileUpdate/ProfileUpdate.js
+++ src/Application/Features/ProfileUpdate/ProfileUpdate.js
@@ -1,13 +1,17 @@
-import StaffService from "../utilities/services/StaffService";
+import StaffService from '../utilities/services/StaffService';
 
 class ProfileUpdate {
   static async profileInfo(req) {
-    const { tenantRef, currentStaff: { staffId }, body } = req;
+    const {
+      tenantRef, currentStaff, currentAdmin, body
+    } = req;
+
+    const requester = currentStaff || currentAdmin;
 
     try {
-      const updated = await StaffService.updateStaffInfo(tenantRef, staffId, body);
+      const updated = await StaffService.updateStaffInfo(tenantRef, requester.staffId, body);
       
-      return [updated ? 200 : 500, `Profile ${updated ? '' : 'not '}updated!`];      
+      return [updated ? 200 : 500, `Profile ${updated ? '' : 'not '}updated!`];
     } catch (e) {
       return [500, 'An error occurred ERR500PRFUPD'];
     }
